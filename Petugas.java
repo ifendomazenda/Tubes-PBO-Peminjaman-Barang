ee07c2da56eea4107b7c220c8f1fb5e577bfd2b1
elrizkimardhi <elrizkimardhi@gmail.com>
2016-03-12 23:42:33 +0700

    Update Petugas.java

--- Petugas.java
+++ Petugas.java
@@ -1,1 +1,45 @@
+package tubes_pbo_kelompok_17;
+
+public class Petugas extends Orang{
+    private String userPetugas;
+    private String passPetugas;
+    
+    public Petugas(int idOrang) {
+        super(idOrang);
+    }
+
+    public void setUserPetugas(String userPetugas){
+        this.userPetugas = userPetugas;
+    }
+
+    public void setPassPetugas(String passPetugas) {
+        this.passPetugas = passPetugas;
+    }
+
+    public String getUserPetugas() {
+        return userPetugas;
+    }
+
+    public String getPassPetugas() {
+        return passPetugas;
+    }
+    
+    public int getIdPetugas(){
+        return super.getIdOrang();
+    }
+    
+    public boolean cekPetugas(String user, String pass){
+        if (this.userPetugas.equals(user)){
+            if (this.passPetugas.equals(pass)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+}
 
