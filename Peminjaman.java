e05df7f63923d857042aa4927d1295f7856253b1
ifendomazenda <ifendo.mazenda@yahoo.com>
2016-03-12 23:46:33 +0700

    Update Peminjaman.java

--- Peminjaman.java
+++ Peminjaman.java
@@ -1,1 +1,82 @@
+package tubes_pbo_kelompok_17;
 
+public class Peminjaman {
+    private Barang[] pinjaman;
+    private Anggota peminjam;
+    private int statusPinjam; // sudah dikembalikan 1, belum dikembalikan 0
+    private String tglKembali;
+    private String tglTelat;
+    private int denda;
+    private int idPinjam;
+
+    public Peminjaman(int idPinjam) {
+        this.idPinjam = idPinjam;
+    }
+
+    public Peminjaman(Barang[] pinjaman, Anggota peminjam) {
+        this.pinjaman = pinjaman;
+        this.peminjam = peminjam;
+    }
+
+    public Peminjaman(Anggota peminjam) {
+        this.peminjam = peminjam;
+    }
+
+    public Barang[] getPinjaman() {
+        return pinjaman;
+    }
+
+    public Anggota getPeminjam() {
+        return peminjam;
+    }
+
+    public int getStatusPinjam() {
+        return statusPinjam;
+    }
+
+    public String getTglKembali() {
+        return tglKembali;
+    }
+
+    public String getTglTelat() {
+        return tglTelat;
+    }
+
+    public int getDenda() {
+        return denda;
+    }
+
+    public int getIdPinjam() {
+        return idPinjam;
+    }
+
+    public void setPinjaman(Barang[] pinjaman) {
+        this.pinjaman = pinjaman;
+    }
+
+    public void setPeminjam(Anggota peminjam) {
+        this.peminjam = peminjam;
+    }
+
+    public void setStatusPinjam(int statusPinjam) {
+        this.statusPinjam = statusPinjam;
+    }
+
+    public void setTglKembali(String tglKembali) {
+        this.tglKembali = tglKembali;
+    }
+
+    public void setTglTelat(String tglTelat) {
+        this.tglTelat = tglTelat;
+    }
+
+    public void setDenda(int denda) {
+        this.denda = denda;
+    }
+
+    public void setIdPinjam(int idPinjam) {
+        this.idPinjam = idPinjam;
+    }
+
+    
+}
